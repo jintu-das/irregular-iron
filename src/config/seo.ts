71727fefc3d700f6876d7748c4d25fb945a0ddb3
Jintu Das <jintudas047@gmail.com>
2025-07-31 22:22:10 +0530

    feat: add SEO configuration and integrate astro-seo for improved metadata handling
    - Added astro-seo package to enhance SEO capabilities.
    - Integrated SEO component in Layout.astro to manage title, description, and Open Graph tags.
    - Created a new SEO configuration file (seo.ts) with relevant metadata for the site.
    - Added a placeholder Header component for future use.

---
+++ src/config/seo.ts
@@ -0,0 +1,20 @@
+export const SEO_CONFIG = {
+  title: "Auditrol",
+  description:
+    "Auditrol revolutionizes risk management with AI, helping financial institutions mitigate regulatory and data risks through automated testing and collaborative workflows.",
+  keywords:
+    "Auditrol, Risk management, AI, Financial institutions, Regulatory risks, Data risks, Automated testing, Collaborative workflows",
+  opengraph: {
+    title: "A Very Descriptive Title",
+    type: "A type.",
+    image:
+      "https://user-images.githubusercontent.com/5182256/131216951-8f74f425-f775-463d-a11b-0e01ad9fce8d.png",
+  },
+  twitter: {},
+  author: "Auditrol",
+  owner: "Auditrol",
+  site_name: " auditrol.com",
+  public_url: "https://auditrol.com",
+  site_url: "https://auditrol.com",
+  social_image: "/static/auditrol_banner.png",
+};
